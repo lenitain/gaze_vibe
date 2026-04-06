196156b76ad2d690e25e1c589a9407ab5d4fb551
lenitain <xt.zhu@qq.com>
2026-04-06 13:35:19 +0800

    remove ~

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -133,7 +133,7 @@ function handleRegionSwitch({ from, to }) {
         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
         </svg>
-        ~/{{ projectFolder.name }}
+        {{ projectFolder.name }}
       </p>
     </header>
 
